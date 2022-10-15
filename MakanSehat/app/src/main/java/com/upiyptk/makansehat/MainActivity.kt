7ce2a5bcf372ab2aa80e546654b2ef4917f3e3c0
nokatotedo <dwikypanduaqri22@gmail.com>
2022-10-15 22:27:29 +0700

    Add progress bar
    Add progress bar to show total kcal of recent food

--- MakanSehat/app/src/main/java/com/upiyptk/makansehat/MainActivity.kt
+++ MakanSehat/app/src/main/java/com/upiyptk/makansehat/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.upiyptk.makansehat
 
 import android.os.Bundle
+import android.widget.ProgressBar
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -10,7 +12,12 @@ import com.google.firebase.database.*
 class MainActivity: AppCompatActivity() {
     private lateinit var ref: DatabaseReference
     private lateinit var rv: RecyclerView
+    private lateinit var progressInner: ProgressBar
+    private lateinit var progressOuter: ProgressBar
+    private lateinit var carbohidat: TextView
     private var array: ArrayList<FoodData> = arrayListOf()
+    private var total: Double = 0.0
+    private var percent: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,15 +27,29 @@ class MainActivity: AppCompatActivity() {
         rv.layoutManager = LinearLayoutManager(this)
         rv.setHasFixedSize(true)
 
+        progressInner = findViewById(R.id.pb_foodRecent_inner)
+        progressOuter = findViewById(R.id.pb_foodRecent_outer)
+        carbohidat = findViewById(R.id.tv_foodRecent_carbohidrat)
+        progressInner.progress = 0
+        progressOuter.progress = 0
+        carbohidat.text = "0 kcal"
+
         ref = FirebaseDatabase.getInstance().getReference("makanan")
         ref.addValueEventListener(object: ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
+                total = 0.0
                 array.clear()
                 if(snapshot.exists()) {
                     for(food in snapshot.children) {
                         val foodValue = food.getValue(FoodData::class.java)
-                        array.add(foodValue!!)
+                        total += foodValue?.karbohidrat!!
+                        array.add(foodValue)
                     }
+                    percent = (total/2).toInt()
+                    progressInner.progress = percent
+                    progressOuter.progress = percent
+                    carbohidat.text = total.toString() + " kcal"
+
                     rv.adapter = FoodAdapter(array)
                 }
             }
